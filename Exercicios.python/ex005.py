1b311252049a2ba0b6bbca5fd7eb527df437acf9
RafaelLanaDS <rafael.lanads@outlook.com>
2025-03-23 13:04:22 -0300

    mudando as pastas de exercicios

---
+++ Exercicios.python/ex005.py
@@ -0,0 +1,6 @@
+#faça um programa que leia um numero inteiro e mostre na tela seu sucessor e seu antecessor.
+
+n = int(input('digite um numero: '))
+a = n-1
+s = n+1
+print('Analisando o valor {}, seu antecessor é {} e o seu sucessor é, {}'.format(n,a,s))
