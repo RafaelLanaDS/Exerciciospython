f0a820264658a4d54f06d87a2e8de4eba4ec8170
RafaelLanaDS <rafael.lanads@outlook.com>
2025-03-25 00:17:13 -0300

    atualizando exercicios
    Hoje foi um saco, meu codigo nao rodo por conta de uma variavel que eu nao criei !!!!

--- Exercicios.python/ex049.py
+++ Exercicios.python/ex049.py
@@ -1,1 +1,7 @@
 '''Refaça o exercicio  009 mostrando a tabuada de um numero que o usuario escolher, so que agora utilizando um laço for.'''
+
+num = int(input('Digite um numero para saber a sua tabuada: '))
+
+for c in range (1, 11):
+    print('{} X {:2} = {}' .format(num, c, num*c))
+print('Tabuada finalizada !!')
--- Exercicios.python/ex050.py
+++ Exercicios.python/ex050.py
@@ -1,1 +1,7 @@
 '''Desenvolva um programa que leia seis numeros inteiros e mostre a soma apenas daquelas que forem pares, se o valor digitado for impar desconsidereo'''
+soma = 0
+for c in range(6):
+    num = int(input('Digite um numero: '))
+    if num %2 == 0:
+        soma = soma + num
+print('A soma dos numeros pares da lista {}' .format(soma))
