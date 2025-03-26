b73df14e71d325ded63f8b32a7db01fe66e216e9
RafaelLanaDS <rafael.lanads@outlook.com>
2025-03-26 00:12:23 -0300

    fiz os exercicios de hoje !!
    fazer revisão do ex  53

--- Exercicios.python/ex052.py
+++ Exercicios.python/ex052.py
@@ -1,1 +1,16 @@
 '''Faça um programa que leia um numero interio e diga se ela é ou não um numero primo'''
+
+n1 = int(input('Digite um numero: '))
+tot = 0
+for c in range(1, n1 + 1):
+    if n1 % c == 0:
+        print('\033[33m', end='')
+        tot = tot + 1
+    else:
+        print('\033[31', end='')
+    print('{}' .format(c), end='')
+print('\n\033[m O numero {} foi divisivel {} vezes' .format(n1, tot))   
+if tot == 2:
+    print('E POR ISSO ELE É PRIMO')
+else:
+    print('E POR ISSO ELE NÃO É PRIMO')
--- Exercicios.python/ex053.py
+++ Exercicios.python/ex053.py
@@ -1,1 +1,32 @@
-'''Crie um programa que leia uma frase qualquer e diga se ela é um polindroma, desconsiderando os espaços'''
+'''Crie um programa que leia uma frase qualquer e diga se ela é um polindroma, desconsiderando os espaços '''
+
+'''def eh_palindromo(frase):
+    # Remove espaços e transforma em minúsculas
+    frase_limpa = ''.join(frase.lower().split())
+    
+    # Verifica se a frase é um palíndromo usando um loop for
+    tamanho = len(frase_limpa)
+    for i in range(tamanho // 2):
+        if frase_limpa[i] != frase_limpa[tamanho - i - 1]:
+            return False
+    return True
+
+# Entrada do usuário
+frase = input("Digite uma frase: ")
+
+# Verificação e saída
+if eh_palindromo(frase):
+    print("A frase {} é um palíndromo!" .format(frase))
+else:
+    print("A frase {} não é um palíndromo." .format(frase)) '''
+
+frase = str(input('Digite uma frase: ')).strip().upper() # strip remove os espaços da frase 
+palavras = frase.split()
+junto =''.join(palavras) #join junta todas as palavras sem seus espaços 
+inverso =''
+for letra in range(len(junto)-1, -1, -1):
+    inverso = inverso + junto[letra]
+if inverso == junto:
+    print('Temos um palindromo')
+else:
+    print('A frase digitada não é um palindromo')
