9c3db55a3732ec29a98b2704fb5291b0b0dcae9e
RafaelLanaDS <rafael.lanads@outlook.com>
2025-03-27 21:48:42 -0300

    ex
    utilizando while como laço de repetição,
    utilizado quando não se sabe quantas vezes será necessario utilizar o laço de repetição

--- Desefios.python/aula013.py
+++ Desefios.python/aula013.py
@@ -1,5 +1,12 @@
-s = 0 
+''' s = 0 
 for c in range (0, 4):
     n =  int(input('Digite um valor: '))
     s = s + n
-print('O somatorio de todos os valores foi {}' .format(s))
+print('O somatorio de todos os valores foi {}' .format(s)) '''
+  
+c = 1
+while c < 10:
+    print(c)
+    c = c + 1
+print('FIm')
+
