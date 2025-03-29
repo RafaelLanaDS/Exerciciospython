db1b652762794fcc4fd69a38a273f8231aed6a41
RafaelLanaDS <rafael.lanads@outlook.com>
2025-03-29 20:38:11 -0300

    subindo ex059

--- Exercicios.python/ex059.py
+++ Exercicios.python/ex059.py
@@ -12,24 +12,43 @@
 [
 Seu programa deverá realizar a operação solicitada em cada caso. '''
 
-n1 = float(input('Digite o primeiro valor: '))
-n2 = float(input('Digite o segundo valor: '))
-print('''
-[ 1 ] somar
+from time import sleep
 
-[ 2 ] multiplicar
+n1 = int(input('Digite o primeiro valor:'))
+n2 = int(input('Digite o segundo valor: '))
+opcao = 0 
+while opcao != 5:
+    print('''
+    [ 1 ] somar
 
-[ 3 ] maior
+    [ 2 ] multiplicar
 
-[ 4 ] novos números
+    [ 3 ] maior
 
-[ 5 ] sair do programa''')
-escolhido = int(input('Qual operçao desseja escolher ?'))
-while escolhido in (1,6):
-    if escolhido == 1:
-        print('A soma entre numero {} e numero {} foi {}' .format(n1, n2, n1 + n2))
-    elif escolhido == 2:
-        print('A multiplo]icação entre {} e {} é {}' .format(n1, n2, n1 * n2))
-    elif escolhido == 3:
-        print('A m')
-print('Acabou')
+    [ 4 ] novos números
+
+    [ 5 ] sair do programa''')
+    opcao = int(input('Qual opção quer escolher: '))
+    if opcao == 1:
+        soma = n1 + n2
+        print('A soma entre {} + {} é {}' .format(n1, n2, soma))
+    elif opcao == 2:
+        multiplicacao = n1 * n2
+        print('O resiltado de {} x {} é {}' .format(n1, n2, multiplicacao))
+    elif opcao == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print('Entre {} e {} o maior é {}' .format(n1, n2, maior))
+    elif opcao == 4:
+        print('Informe um novo valor')
+        n1 = int(input('Digite o primeiro valor:'))
+        n2 = int(input('Digite o segundo valor: '))
+    elif opcao == 5:
+        print('Fizalizando...')
+        sleep(2)
+    else:
+        print('Opção invalida tente novamente')
+    print('-=-' * 10)
+print('fim do programa')
