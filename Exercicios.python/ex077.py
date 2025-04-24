cfc956bc971785e53da833793dc41691aff41b6f
RafaelLanaDS <rafael.lanads@outlook.com>
2025-04-24 19:41:36 -0300

    atualizando exercicios

--- Exercicios.python/ex077.py
+++ Exercicios.python/ex077.py
@@ -4,6 +4,10 @@ Crie um programa que tenha uma tupla com vários palavras(não usar acentos). De
 
 palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
 
-
-
+for c in palavras:
+    print('\nNa palavra {} temos'.format(c.upper()), end=' ')
+    for letra in c:
+        if letra.lower()  in 'aeiou':
+            print(letra, end=' ')
+        
 
