e2cd10f8cb45ef38fdb09c34acfb3238144394a7
RafaelLanaDS <rafael.lanads@outlook.com>
2025-05-21 23:31:14 -0300

    21/05

---
+++ Exercicios.python/ex098.py
@@ -0,0 +1,6 @@
+'''
+Faça um programa que tenha uma função chamada  contador()
+quereceba tres parametros,inicio, fim e passo
+seu programa tem que realizar tres contagens atraves da função criada 
+'''
+
