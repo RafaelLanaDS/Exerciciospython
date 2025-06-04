65c014e6e53203c17acc3a48bf448bb60b0aacce
RafaelLanaDS <rafael.lanads@outlook.com>
2025-06-03 22:52:44 -0300

    03/06

--- Exercicios.python/ex001.py
+++ Exercicios.python/ex001.py
@@ -1,1 +1,15 @@
+num = [2,8,4,7]
+num.pop()
+num.insert(1,3)
+num.append(6)
+print(num)
 
+letras = ('J', 'X', 'M', 'O', 'A', 'K')
+print(letras.index('A'))
+
+n = (1,2,3,4,5,6,7,8)
+print(sorted(n))
+print(n[0], n[-1])
+
+pessoas = [['Pedro', 25], ['Maria', 12], ['José', 25]]
+print(pessoas[2][1])
